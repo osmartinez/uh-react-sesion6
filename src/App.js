a7b6a738617f566cbff8a50f2957be8a39c077b6
Oscar <oscarmtzz22@gmail.com>
2023-03-15 20:22:01 +0100

    wip store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Route, Routes } from 'react-router-dom';
+import NavigationComponent from './components/NavigationComponent/NavigationComponent'
+import AboutPage from './pages/AboutPage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import PostsPage from './pages/PostsPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationComponent></NavigationComponent>
+      <Routes>
+        <Route exact path="/" element={<HomePage/>}></Route>
+        <Route exact path="/posts" element={<PostsPage/>}></Route>
+        <Route exact path="/about" element={<AboutPage/>}></Route>
+        <Route exact path="/login" element={<LoginPage/>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/MultiPostsComponent/MultiPostsComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const MultiPostsComponent = () => (
+  <div>
+    MultiPostsComponent Component
+  </div>
+);
+
+MultiPostsComponent.propTypes = {};
+
+MultiPostsComponent.defaultProps = {};
+
+export default MultiPostsComponent;
